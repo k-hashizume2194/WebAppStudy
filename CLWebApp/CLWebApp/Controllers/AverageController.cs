f3419c467ce6f295cf6fa214232fba56202d28e3
n-hashimoto2193 <n-hashimoto@chancelab.jp>
2018-10-24 11:58:18 +0900

    打率計算画面の資材登録

---
+++ CLWebApp/CLWebApp/Controllers/AverageController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CLWebApp.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CLWebApp.Controllers
+{
+    public class AverageController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "打率計算アプリ";
+            AverageViewModel model = new AverageViewModel();
+            return View(model);
+        }
+    }
+}
