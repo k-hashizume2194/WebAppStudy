60e978647b113cb90e201d3f9df20f920472f24b
n-hashimoto2193 <n-hashimoto@chancelab.jp>
2018-10-24 14:24:39 +0900

    燃費計算資材登録

---
+++ CLWebApp/CLWebApp/Controllers/NenpiController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CLWebApp.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CLWebApp.Controllers
+{
+    public class NenpiController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "燃費計算アプリ";
+            NenpiViewModel model = new NenpiViewModel();
+            return View(model);
+        }
+    }
+}
