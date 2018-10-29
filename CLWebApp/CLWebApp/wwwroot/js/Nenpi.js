6411c67f95abdddd32fc6e1c66565caa0a4f219e
k-hashidume <k-hashidume@chancelab.jp>
2018-10-29 15:24:21 +0900

    マイグレーションやり直し＋燃費計算ボタン制御のjs化

--- CLWebApp/CLWebApp/wwwroot/js/Nenpi.js
+++ CLWebApp/CLWebApp/wwwroot/js/Nenpi.js
@@ -1,1 +1,15 @@
-﻿
+﻿// Write your JavaScript code.
+// HTMLの読み込みが完了したら動く
+$(function () {
+
+   // btnCalculationEnabledがfalseなら計算ボタンをdisabledにする
+
+   // ①btnCalculationEnabledの値をとる
+    var hiddenVal = $('#btnCalculationEnabled').val();
+   // 文字列のboolean判定
+   var btnCalculationEnabledval = hiddenVal.toLowerCase() === "true";
+
+   // ②ボタンのdisabledを①の値をもとに切り替え]
+   $('#calcBtn').prop('disabled', !btnCalculationEnabledval);
+
+});
