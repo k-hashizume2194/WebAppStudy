adecd15713f59e4c3b4b9f58e14f7b300ac64a01
DESKTOP-B89OIF4\user <t-hashimoto@chancelab.jp>
2018-11-03 10:13:38 +0900

    site.jsのバグ修正＆燃費計算画面の不要id指定削除

--- CLWebApp/CLWebApp/wwwroot/js/site.js
+++ CLWebApp/CLWebApp/wwwroot/js/site.js
@@ -2,11 +2,18 @@
 // HTMLの読み込みが完了したら動く
 $(function () {
     //テキストボックスにフォーカスが当たった時に実行
-    $("input[type='text']").focus()
-        .click(function () {
-            $(this).select();
-            return false;
-        });
+    //$("input[type='text']").focus()
+    //    .click(function () {
+    //        $(this).select();
+    //        return false;
+    //    });
+    // ※↑この書き方ではfocusが当たった箇所を自動的にクリックしてしまう
+
+    // テキストボックスにフォーカスが当たった時に実行
+    $("input[type='text']").on('click focus', function () {
+        $(this).select();
+        return false;
+    });
 
     $('.datepicker').datepicker({
         autoclose: 'true',
