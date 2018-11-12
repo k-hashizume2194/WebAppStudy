c6bc11b40d8088f4054d295c3911fef3f9c015b0
k-hashidume <k-hashidume@chancelab.jp>
2018-11-12 17:53:28 +0900

    アノテーション追加、confirm追加

--- CLWebApp/CLWebApp/Models/ViewModels/Practice1ViewModel.cs
+++ CLWebApp/CLWebApp/Models/ViewModels/Practice1ViewModel.cs
@@ -8,12 +8,14 @@ namespace CLWebApp.Models.ViewModels
 {
     public class Practice1ViewModel
     {
+        [Range(0.01, 3.00, ErrorMessage = "身長は0より大きい数値(最大：3.00)、入力してください")]
         [Required(ErrorMessage = "身長を入力してください")]
-        [Display(Name = "身長：")]
+        [Display(Name = "身長(m)：")]
         public string height { get; set; }
 
+        [Range(0.1, 999.9, ErrorMessage = "体重は0より大きい数値(最大：999.9)で入力してください")]
         [Required(ErrorMessage = "体重を入力してください")]
-        [Display(Name = "体重：")]
+        [Display(Name = "体重(kg)：")]
         public string weight { get; set; }
 
         [Display(Name = "BMI：")]
---
+++ CLWebApp/CLWebApp/wwwroot/js/Practice1.js
@@ -0,0 +1,8 @@
+﻿    // Write your JavaScript code.
+    // HTMLの読み込みが完了したら動く
+    $(function () {
+        $('#calcbmi').on('click', function () {
+            window.confirm('計算処理を実行します。よろしいですか？');
+    });
+
+    });
