cab991d380588d21069baa11ac849b1c1eb13729
n-hashimoto2193 <n-hashimoto@chancelab.jp>
2018-11-15 12:10:02 +0900

    コメント追記

--- CLWebApp/CLWebApp/wwwroot/js/Practice2.js
+++ CLWebApp/CLWebApp/wwwroot/js/Practice2.js
@@ -1,9 +1,8 @@
 ﻿// Write your JavaScript code.
 // HTMLの読み込みが完了したら動く
 $(function () {
-    //var DrawVal = $('#Draw').val();
-    //var DrawNum = Number(DrawVal);
 
+    // 引き分け数が""の時、フォーカスアウト時にゼロをセット
     $('#Draw').blur(function () {
         var DrawVal = $('#Draw').val();
         if (DrawVal == "") {
@@ -11,7 +10,7 @@ $(function () {
         }
     });
 
-    
+    // 勝率が".000"の時、勝率テキストボックス、文字の色を変更
     var WinningVal = $('#Winning').val();
     if (WinningVal == ".000")
     {
@@ -19,6 +18,7 @@ $(function () {
         $("#Winning").css('color', '#770000');
     }
 
+    // 勝率が".499"以下の時、勝率テキストボックス、文字の色を変更
     var WinningNum = Number(WinningVal);
     if (WinningNum == "")
     {
